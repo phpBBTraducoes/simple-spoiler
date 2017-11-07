c2bdb754f3dfe4ea12876c0ac2a48155d8e3cd4d
phpBBTraducoes <eunaumtenhoid@outlook.com>
2017-11-07 09:56:58 -0200

    Traducao ADD

---
+++ language/pt_br/simplespoiler.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Simple Spoiler Extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2017 Alfredo Ramos
+ * @license GNU GPL-2.0
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.9] (https://github.com/phpBBTraducoes)
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'SPOILER'			=> 'Spoiler',
+	'SPOILER_HELPLINE'	=> 'Uso: [spoiler]texto[/spoiler] ou [spoiler=título]texto[/spoiler]',
+	'SPOILER_SHOW'		=> 'Mostrar',
+	'SPOILER_HIDE'		=> 'Esconder'
+]);
