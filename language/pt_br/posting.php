9bffa6fa2544e1552df5c3472d2e12696d4ab17e
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-01-24 09:21:17 -0200

    UPDATE

--- language/pt_br/posting.php
+++ language/pt_br/posting.php
@@ -2,10 +2,10 @@
 
 /**
  * Simple Spoiler Extension for phpBB.
+ * Brazilian Portuguese translation by eunaumtenhoid [2017][ver 1.3.0] (https://github.com/phpBBTraducoes)
  * @author Alfredo Ramos <alfredo.ramos@yandex.com>
  * @copyright 2017 Alfredo Ramos
- * @license GPL-2.0
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.2.0] (https://github.com/phpBBTraducoes)
+ * @license GPL-2.0-only
  */
 
 /**
